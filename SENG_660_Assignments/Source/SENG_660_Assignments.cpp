0d53deb6aaa4439e750229a939db15b0e5c1f2e3
erssenor <erssenor@gmail.com>
2025-06-24 19:37:54 -0700

    Signed commit 3

--- SENG_660_Assignments/Source/SENG_660_Assignments.cpp
+++ SENG_660_Assignments/Source/SENG_660_Assignments.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 
-	std::cout << "Signed commit, again\n";
+	std::cout << "Signed commit, again 3\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
